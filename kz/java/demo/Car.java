07c0ea3a86246ad4068636a2a6bd7261c4799c0e
Computer <computer@dell-g15-5511.jits.ipa.dc>
2023-07-05 15:11:12 +0600

    created car class fields and methods

--- kz/java/demo/Car.java
+++ kz/java/demo/Car.java
@@ -3,5 +3,13 @@ package kz.java.demo;
 public class Car {
     String name;
     String model;
+    String color;
 
+    public String drive() {
+        return null;
+    }
+
+    public String stop() {
+        return null;
+    }
 }
--- kz/java/demo/Main.java
+++ kz/java/demo/Main.java
@@ -3,6 +3,13 @@ package kz.java.demo;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        Car bmw = new Car();
+        Car nissan = new Car();
+
         System.out.println(car.model);
+        System.out.println(bmw.drive());
+        System.out.println(nissan.stop());
+
+
     }
 }

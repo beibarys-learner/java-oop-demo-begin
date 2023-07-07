02f48bb27c96d00d042b6b9741f039fdb7d38ce4
Computer <computer@dell-g15-5511.jits.ipa.dc>
2023-07-07 21:34:56 +0600

    created Engine class

---
+++ kz/java/demo/Engine.java
@@ -0,0 +1,13 @@
+package kz.java.demo;
+
+public class Engine {
+    private double Volume;
+
+    public double getVolume() {
+        return Volume;
+    }
+
+    public void setVolume(double volume) {
+        Volume = volume;
+    }
+}

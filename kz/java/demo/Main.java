0a00cafd8b179661fc0cca86e48257076f450c79
Computer <computer@dell-g15-5511.jits.ipa.dc>
2023-07-07 19:58:36 +0600

    add toyota object with child class inherited from car and transport classes

--- kz/java/demo/Main.java
+++ kz/java/demo/Main.java
@@ -2,23 +2,10 @@ package kz.java.demo;
 
 public class Main {
     public static void main(String[] args) {
-
-        Car car1 = new Car("a", "b", "c");
-        // a b c values are derived to Transport
-        // a b c and 10 goes to Car, not Transport
-        Car car3 = new Car("a", "b", "c", 10);
-        Car car = new Car( 10);
-
-        System.out.println("car.getName() = " + car.getName());
-        System.out.println("car.getColor() = " + car.getColor());
-        System.out.println("car.getModel() = " + car.getModel());
-        System.out.println("car.getNumber() = " + car.getNumber());
-
-        //Car class inherits all methods and fields from parent class Transport, and has access to them
-     /*   car.setName("car");
-        car.drive();*/
-
-//        transport.signal(); // if you uncomment it, notice that transport object doesn't have unique fields and methods of car
+        Toyota toyota = new Toyota();
+        toyota.signal();
+        // toyota has all car's and transport functionalities
+        // go Transport class and press ctrl + H
 
     }
 }

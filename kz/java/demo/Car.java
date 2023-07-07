94fc32bd1817cd1825dd7c16212e375f28392ffc
Computer <computer@dell-g15-5511.jits.ipa.dc>
2023-07-07 18:11:31 +0600

    added number field for car, and some methods in main class

--- kz/java/demo/Car.java
+++ kz/java/demo/Car.java
@@ -1,5 +1,17 @@
 package kz.java.demo;
 
 public class Car extends Transport{
+    private int number; // car can have own number so Car class also has get, set methods
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void signal(){
+        System.out.println("signal");
+    }
 }
--- kz/java/demo/Main.java
+++ kz/java/demo/Main.java
@@ -8,5 +8,15 @@ public class Main {
         //Car class inherits all methods and fields from parent class Transport, and has access to them
         car.setName("car");
         car.drive();
+
+        //new car functional features
+        car.signal();
+        car.setNumber(12);
+        System.out.println(car.getNumber());
+
+        Transport transport = new Transport();
+
+//        transport.signal(); // if you uncomment it, notice that transport object doesn't have unique fields and methods of car
+
     }
 }
